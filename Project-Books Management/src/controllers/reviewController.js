293273650a1dc41ec599e1675ced7bb6e7d6c607
SONU VERMA <99230810+sonu517825@users.noreply.github.com>
2022-03-31 11:19:06 +0530

    Update reviewController.js

--- Project-Books Management/src/controllers/reviewController.js
+++ Project-Books Management/src/controllers/reviewController.js
@@ -41,11 +41,15 @@ const createReview = async function (req, res) {
     try {
 
 
+     
+
+
         let paramsBookId = req.params.bookId // make sure you pass object id in path var. without any quote else not consider
         if (!isObjectId(paramsBookId)) return res.status(400).send({ status: false, msg: "your path params book id must be a object Id" })
         let isBookPresent = await bookModel.findOne({ _id: paramsBookId, isDeleted: false })
         if (!isBookPresent) return res.status(400).send({ status: true, msg: "book is not present may be book is deleted" })
         // we can also handle indivisual    comming soon
+
 
         if (isValidRequestBody(req.query)) return res.status(400).send({ status: false, msg: "You can not pass query" })
 
@@ -56,12 +60,13 @@ const createReview = async function (req, res) {
         if (!isValidRequestBody(data)) return res.status(400).send({ status: false, message: "review data is missing in request body" })
 
 
-        const { bookId, reviewedBy, reviewedAt, rating, review, isDeleted } = data
+        let { bookId, reviewedBy, reviewedAt, rating, review, isDeleted } = data
 
 
-        let book_id = bookId.trim() // here you can pass space
+        let book_id = bookId.replaceAll(" ", "")
         if (!isValid(book_id)) return res.status(400).send({ status: false, message: "bookId is required" })
         if (!isObjectId(book_id)) return res.status(400).send({ status: false, msg: "your request body book id must be a object Id" })
+        data.bookId = book_id // no need 
 
 
         if (paramsBookId !== book_id) return res.status(400).send({ status: false, msg: "make sure path variable book Id must be same your request body book Id" })
@@ -70,9 +75,7 @@ const createReview = async function (req, res) {
 
         if (paramsBookId === book_id) {
             let _id = isBookPresent._id
-            data.bookId = _id
-
-            console.log(_id, data)
+            data.bookId = _id   // here we assign
 
 
             if (!isValid(reviewedBy)) return res.status(400).send({ status: false, message: "reviewedBy is required" })
@@ -88,10 +91,11 @@ const createReview = async function (req, res) {
 
 
             // if (!isValid(review)) return res.status(400).send({ status: false, message: "review is required" })
-            if (review) { // not accept only quote but store in schema have to handle comming soon
+            if (review.trim().length==0) { // not accept only quote but store in schema have to handle comming soon
                 if (!isValid(review)) return res.status(400).send({ status: false, msg: "please provide proper 'review' key " })
                 if (!isString(review)) return res.status(400).send({ status: false, msg: "please provide review in 'string' case only" })
             }
+
 
             if (isDeleted) {  // not accept only quote must be a space or character
                 if (!isValid(isDeleted)) return res.status(400).send({ status: false, msg: "please provide proper 'isDeleted' key " })
@@ -99,15 +103,20 @@ const createReview = async function (req, res) {
                 if (isDeleted == true) return res.status(400).send({ status: false, msg: "You can not create Deleted review" })
             }
 
+
             const saveData = await reviewModel.create(data)
+
 
             if (saveData) {
                 let reviews = isBookPresent.reviews
                 reviews = reviews + 1
                 await bookModel.findOneAndUpdate({ _id: _id }, { reviews: reviews }, { new: true })
             }
+
+
             return res.status(201).send({ status: true, message: "Data Created Successfully", data: saveData })
-            // here we not populate with book data can see only review model comming soon
+            // here we not populate with book data we can see only review model populate comming soon
+
         }
     } catch (err) {
         res.status(500).send({ status: false, error: err.message })
@@ -117,8 +126,10 @@ const createReview = async function (req, res) {
 
 
 
+
 const updatedReview = async function (req, res) {
     try {
+
 
         let paramsBookId = req.params.bookId // make sure you pass object id in path var. without any quote else not consider
         if (!isObjectId(paramsBookId)) return res.status(400).send({ status: false, msg: "your path params book id must be a object Id" })
@@ -130,7 +141,7 @@ const updatedReview = async function (req, res) {
         let paramsReviewId = req.params.reviewId // make sure you pass object id in path var. without any quote else not consider
         if (!isObjectId(paramsReviewId)) return res.status(400).send({ status: false, msg: "your path params review id must be a object Id" })  // not consider last char if char than put char
         let isReviewPresent = await reviewModel.findOne({ _id: paramsReviewId, isDeleted: false })
-        if (!isReviewPresent) return res.status(400).send({ status: true, msg: "Review is not present may be book is deleted" })
+        if (!isReviewPresent) return res.status(400).send({ status: true, msg: "Review is not present may be review is deleted" })
         // we can also handle indivisual    comming soon
 
 
@@ -143,16 +154,23 @@ const updatedReview = async function (req, res) {
         const newReviewedBy = data.reviewedBy
 
 
-        let filters = Object.entries(data)
+        // work only string
+        if (newReview.trim().length==0) { // not accept only quote but store in schema have to handle comming soon
+            if (!isValid(newReview)) return res.status(400).send({ status: false, msg: "please provide proper 'review' key " })
+            if (!isString(newReview)) return res.status(400).send({ status: false, msg: "please provide review in 'string' case only" }) // no need
+        }
 
 
-        if (!isValidRequestBody(data)) return res.status(400).send({ status: false, message: "Please provide Update Data in request body" })
+        if (newRating) { // not accept only quote but store in schema have to handle comming soon
+            if (!isValid(newRating)) return res.status(400).send({ status: false, msg: "please provide proper 'rating' key " })
+            if (!isNumber(newRating)) return res.status(400).send({ status: false, msg: "please provide rating in 'number' case only" })
+            if ((newRating < 1 || newRating > 5)) return res.status(400).send({ status: false, message: "ratings should be in 1 to 5" })
+        }
 
-        if (Object.keys(data).length != 0) {
-            let emptyInput = filters.filter((ele) => ele[1] == '')
-            if (emptyInput.length != 0) {
-                return res.status(400).send({ status: false, msg: "You can not pass empty key value" })
-            }
+
+        if (newReviewedBy.trim().length==0) { // not accept only quote but store in schema have to handle comming soon
+            if (!isValid(newReviewedBy)) return res.status(400).send({ status: false, msg: "please provide proper 'reviewedBy' key " })
+            if (!isString(newReviewedBy)) return res.status(400).send({ status: false, msg: "please provide reviewedBy in 'string' case only" }) // no need
         }
 
 
@@ -177,6 +195,7 @@ const deletedReview = async function (req, res) {
     try {
 
         if (isValidRequestBody(req.query)) return res.status(400).send({ status: false, msg: "You can not pass query" })
+//        if (isValidRequestBody(req.body)) return res.status(400).send({ status: false, msg: "You can not pass request body" })
 
         if (isValidRequestBody(req.body)) return res.status(400).send({ status: false, msg: "You can not pass body" })
         const bookId = req.params.bookId
@@ -188,17 +207,27 @@ const deletedReview = async function (req, res) {
 
 
         if (findBook.isDeleted === false) {
+            
             const findReview = await reviewModel.findById(reviewId)
 
+
             if (!findReview) return res.status(400).send({ status: false, message: " ReviewId is not present" })
+
 
             if (findReview.isDeleted === false) {
 
                 const deleteReview = await reviewModel.findOneAndUpdate({ _id: reviewId }, { isDeleted: true }, { new: true })  // deleteAt: Date.now() not in schema
                 return res.status(202).send({ status: true, message: "Data Deleted Successfully", data: deleteReview })
-            } else return res.status(400).send({ status: false, message: "reviewData is already Deleted" })
+            } 
+            
+            else return res.status(400).send({ status: false, message: "reviewData is already Deleted" })
+
         }
+
+
         else return res.status(400).send({ status: false, message: "BookData is already Deleted" })
+
+
     } catch (err) {
         return res.status(500).send({ error: err.message })
     }
